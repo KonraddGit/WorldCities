1b0adf5a60aab6b3aa407c8757c394390352656d
KonraddGit <konradparchanski@gmail.com>
2023-02-07 11:26:40 +0100

    Persistence and Domain layers

---
+++ src/WorldCities.Domain/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorldCities.Domain.Entities;
+
+public class Country
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string ISO2 { get; set; } = null!;
+
+    public string ISO3 { get; set; } = null!;
+
+    public ICollection<City> Cities { get; set; } = null!;
+}
