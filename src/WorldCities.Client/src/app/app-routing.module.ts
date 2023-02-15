3b044b101c49d5089fe5167c7e9b9e4932a6ea17
KonraddGit <konradparchanski@gmail.com>
2023-02-15 15:08:06 +0100

    Remove false component

--- src/WorldCities.Client/src/app/app-routing.module.ts
+++ src/WorldCities.Client/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { CitiesComponent } from './cities/cities.component';
 import { CountriesComponent } from './countries/countries.component';
-import { CityEditComponent } from './city-edit/city-edit.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'cities', component: CitiesComponent },
   { path: 'countries', component: CountriesComponent },
-  { path: 'city-edit', component: CityEditComponent },
 ];
 
 @NgModule({
