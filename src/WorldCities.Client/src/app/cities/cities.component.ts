d18dab46821529d9c662cf9f49f0a99581d2d88b
KonraddGit <konradparchanski@gmail.com>
2023-02-16 13:23:31 +0100

    Introduce debouncing to filter

--- src/WorldCities.Client/src/app/cities/cities.component.ts
+++ src/WorldCities.Client/src/app/cities/cities.component.ts
@@ -3,9 +3,10 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-
 import { environment } from './../../environments/environment';
 import { City } from './city';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cities',
@@ -27,10 +28,25 @@ export class CitiesComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
+  filterTextChanged: Subject<string> = new Subject<string>();
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.loadData();
+  }
+
+  // debounce filter text changes
+  onFilterTextChanged(filterText: string) {
+    if (this.filterTextChanged.observers.length === 0) {
+      this.filterTextChanged
+        .pipe(debounceTime(1000), distinctUntilChanged())
+        .subscribe((query) => {
+          this.loadData(query);
+        });
+    }
+
+    this.filterTextChanged.next(filterText);
   }
 
   loadData(query?: string) {
